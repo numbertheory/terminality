ef7e3d36fc903de5f2827c83b5ffe4e18e856824
JP Etcheber <jetcheber@gmail.com>
2022-01-04 22:47:55 -0800

    wip: trying to make a table and failing

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # terminality
-Terminal tools that I wished into existence. Use at your own discretion.
+Terminal functions for macOS that I wished into existence. Use at your own discretion.
+
+They can all be run independently, but to install them, you'd want to include just the function
+in your shell's run commands (rc) file. Don't copy/paste the whole file, as each file also contains
+a demo of actually running the function.
+
+# Contents
+
+ - [list_mimetypes](list_mimetypes.sh):
+
+    Like a standard `ls -l`, but instead of file permissions, you see the mime-types of
+    each file.
---
+++ list_mimetypes.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash -x
+
+function mimes () {
+  listing_dir="$(ls -ma)"
+  files=("${(@s/,/)listing_dir}")
+  longest_mimetype="0"
+  longest_filename="0"
+  longest_filesize="0"
+  for file in $files;
+    do
+      file_path=$(echo -e "${PWD}/$file" | tr -d '[:space:]')
+      mimetype=$(file --mime-type -b $file_path)
+      file_size=$(du -sh $file_path | awk '{print $1;}')
+      if (( ${#mimetype} > $longest_mimetype)); then
+        longest_mimetype=${#mimetype}
+      fi
+      if (( ${#file} > $longest_filename)); then
+        longest_filename=${#file}
+      fi
+      if (( ${#file_size} > $longest_filesize)); then
+        longest_filesize=${#file_size}
+      fi
+    done
+  for file in $files;
+    do
+      file_path=$(echo -e "${PWD}/$file" | tr -d '[:space:]')
+      mimetype=$(file --mime-type -b $file_path)
+      file_size=$(du -sh $file_path | awk '{print $1;}')
+      echo -n $mimetype
+      for i in {${#mimetype}..${longest_mimetype}}
+        do
+          echo -n " "
+        done
+      if [ "${file}" != "." -a "${file}" != " .." ]; then
+        echo -n "$file_size"
+      else
+        echo -n "    "
+      fi
+      for i in {${#file_size}..${longest_filesize}}
+        do
+          echo -n " "
+        done
+      echo -e "$(echo -e "${file}" | tr -d '[:space:]')"
+    done
+
+}
+
+# Demo line, do not copy below this point
+mimes
