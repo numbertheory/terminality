339e1a5c33e5040fd59c77c0ffd61b3c518ed8e6
JP Etcheber <jetcheber@gmail.com>
2022-01-30 16:54:44 -0800

    gitignore writer

--- gitignore_gen.sh
+++ gitignore_gen.sh
@@ -2,15 +2,41 @@
 
 # Generates a gitignore from the main branch of GitHub's gitignore project
 # https://github.com/github/gitignore
+#
+# By default, it will not overwrite your .gitignore, unless your second argument
+# is "-o"
+#
+# Example: gitignore_gen python -o
+#
+# Note: The script automatically capitalizes the first letter of the language for you,
+# however, it will not automaticaly correct mispellings or other wrong capitalizations.
+
+function restResponse () {
+  curl --write-out "%{http_code}\n" --silent --output /dev/null "$1"
+}
+
+function writeOutGitIgnore () {
+  detectFile=$(restResponse "https://raw.githubusercontent.com/github/gitignore/main/${1}.gitignore")
+  if [[ "$detectFile" == "200" ]]; then
+    curl -s "https://raw.githubusercontent.com/github/gitignore/main/${language}.gitignore" > .gitignore
+    echo "Done writing .gitignore!"
+  else
+    echo "${language} language not found. Response from server: ${detectFile}"
+    echo "See https://github.com/github/gitignore for details."
+  fi
+}
 
 function gitignore () {
   language=${(C)1}
   echo "Generating gitignore for ${(C)language}"
   if [[ -f ".gitignore" ]] && [[ $2 == "-o" ]]; then
     echo "Warning: Overwriting existing gitignore file!"
-    curl -s "https://raw.githubusercontent.com/github/gitignore/main/${language}.gitignore" > .gitignore
-  else if [[ -f ".gitignore" ]];
+    writeOutGitIgnore $language
+  elif [[ -f ".gitignore" ]]; then
     echo "Not overwriting current .gitignore file"
+  else
+    echo "No .gitignore found, writing a new file."
+    writeOutGitIgnore $language
   fi
 
 }
